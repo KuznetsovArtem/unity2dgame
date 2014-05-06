64d26a47ef3dbb3dfed7947acc6ad2d3b16e761b
Artem <artem@gypost.com>
2014-05-06 13:31:17 +0300

    simple enemy

---
+++ Assets/Scripts/MoveScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var speed : Vector2 = new Vector2(10, 10);
+var direction : Vector2 = new Vector2(-1, 0);
+private var movement : Vector2;
+
+function Start () {
+
+}
+
+function Update () {
+	movement = new Vector2(
+		speed.x * direction.x,
+		speed.y * direction.y);
+}
+
+function FixedUpdate() {
+	rigidbody2D.velocity = movement;
+}
---
+++ Assets/Scripts/PlayerScript.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var speed : Vector2 = new Vector2(50, 50);
+private var movement : Vector2;
+
+function Start () {
+
+}
+
+function Update () {
+	var inputX : float = Input.GetAxis("Horizontal");
+	var inputY : float = Input.GetAxis("Vertical");
+	
+	// Movement
+	movement = new Vector2(
+		speed.x * inputX,
+		speed.y * inputY);
+}
+
+function FixedUpdate() {
+	rigidbody2D.velocity = movement;
+}
