2097d35778730f2c6111e2b277c685371bbddd9a
Artem <artem@gypost.com>
2014-05-07 14:36:41 +0300

    enemy shot

---
+++ Assets/Scripts/EnemyScript.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+private var weapon :WeaponScript;
+
+function Awake() {
+	weapon = GetComponent(WeaponScript);
+}
+
+function Update () {
+	if (weapon != null && weapon.CanAttack) {
+		weapon.Attack(true);
+	}
+}
