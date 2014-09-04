aa0a5d78f784e41e92377e0a275ef23d0f92af61
Artem <artem@gypost.com>
2014-09-04 16:58:59 +0300

    tds

---
+++ Assets/Scripts/TDSPlayerScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var health = 100;
+var shootingCount = 0;
+
+
+function Start () {
+
+}
+
+function Update () {
+
+	// Shooting
+	var shoot = Input.GetButtonDown("Fire1")
+		|| Input.GetButtonDown("Fire2");
+	
+	if (shoot) {
+		shootingCount++;
+	}
+}
