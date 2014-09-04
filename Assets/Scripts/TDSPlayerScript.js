ee04cda62225fd4dcc1c2ed51d5648607c9a4173
Artem <artem@gypost.com>
2014-09-04 18:58:29 +0300

    tds

--- Assets/Scripts/TDSPlayerScript.js
+++ Assets/Scripts/TDSPlayerScript.js
@@ -2,19 +2,20 @@
 
 var health = 100;
 var shootingCount = 0;
+var weapon : Transform;
 
 
 function Start () {
-
+	Debug.Log("start fx");
 }
 
 function Update () {
-
 	// Shooting
 	var shoot = Input.GetButtonDown("Fire1")
 		|| Input.GetButtonDown("Fire2");
 	
 	if (shoot) {
+		weapon.animation.Play("attack");
 		shootingCount++;
 	}
 }
