1cba6c3dcb72755d297b5b1023c39b326593d9c1
Artem <artem@gypost.com>
2014-09-05 15:17:47 +0300

    camera

--- Assets/Scripts/TDSPlayerScript.js
+++ Assets/Scripts/TDSPlayerScript.js
@@ -3,13 +3,18 @@
 var health = 100;
 var shootingCount = 0;
 var weapon : Transform;
+var cam : Transform;
 
 
 function Start () {
-	Debug.Log("start fx");
+	Debug.Log(cam.position);
+	cam.position[0] = 20;
+	Debug.Log(cam.position);
 }
 
 function Update () {
+
+	updateCam();
 	// Shooting
 	var shoot = Input.GetButtonDown("Fire1")
 		|| Input.GetButtonDown("Fire2");
@@ -18,4 +23,21 @@ function Update () {
 		weapon.animation.Play("attack");
 		shootingCount++;
 	}
+}
+
+function updateCam() {
+	var camNextLocation : Vector3 = Vector3(
+		this.gameObject.transform.position[0], 
+		15,
+		this.gameObject.transform.position[2]
+	);
+	//var camNextLocation : Vector3 = this.gameObject.transform.position;
+	//Debug.Log(camNextLocation);
+	Camera.mainCamera.transform.position = camNextLocation;
+}
+
+function MoveCamera() 
+{
+    // directly move the Main Camera
+    //Camera.mainCamera.transform.position = TellCamNextLocation;
 }
