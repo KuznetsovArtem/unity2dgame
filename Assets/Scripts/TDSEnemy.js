eab6231b7122f35a1b52b5151c49e30f17727fe2
Artem <artem@gypost.com>
2014-09-11 17:08:05 +0300

    enemy

---
+++ Assets/Scripts/TDSEnemy.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+var health : int = 100;
+
+function applyDammage(demmage: int) {
+	health -= demmage;
+	Debug.Log('Enemy: Ou oi');
+}
--- Assets/Scripts/TDSMelee.js
+++ Assets/Scripts/TDSMelee.js
@@ -2,10 +2,21 @@
 
 var demmage : int = 50;
 var distance : float;
+var maxDistance : float = 1.5;
+var shootingCount = 0;
 
 function Update () {
 	if(Input.GetButtonDown("Fire1")) {
-	//	var hit : RayCastHit;
+		var hit : RaycastHit;
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit)) {
+			distance = hit.distance;
+			if (distance < maxDistance) {
+				Debug.Log('Shoot');
+				hit.transform.SendMessage("applyDammage", demmage, SendMessageOptions.DontRequireReceiver);		
+				shootingCount++;		
+			}
+
+		}
 		
 	}
 }
