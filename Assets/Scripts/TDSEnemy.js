d18082b34d44534761af1a193a96e0a4cf6952b3
Artem <artem@gypost.com>
2014-09-12 13:10:47 +0300

    animation

--- Assets/Scripts/TDSEnemy.js
+++ Assets/Scripts/TDSEnemy.js
@@ -2,7 +2,17 @@
 
 var health : int = 100;
 
+function Update() {
+	if(health <= 0) {
+		Dead();
+	}
+}
+
 function applyDammage(demmage: int) {
 	health -= demmage;
 	Debug.Log('Enemy: Ou oi');
+}
+
+function Dead() {
+	Destroy(gameObject);
 }
--- Assets/Scripts/TDSMelee.js
+++ Assets/Scripts/TDSMelee.js
@@ -3,10 +3,12 @@
 var demmage : int = 50;
 var distance : float;
 var maxDistance : float = 1.5;
+var weapon : Transform;
 var shootingCount = 0;
 
 function Update () {
 	if(Input.GetButtonDown("Fire1")) {
+		weapon.animation.Play("Attack2");
 		var hit : RaycastHit;
 		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit)) {
 			distance = hit.distance;
